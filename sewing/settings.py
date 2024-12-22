f175bc9a0f2fe3567f53a2ccd195d4918cf9e178
Teddy <teddychinomona@gmail.com>
2024-12-23 00:05:06 +0200

    sq;ite dab

--- sewing/settings.py
+++ sewing/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'sewing.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-
     'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-       'NAME':  'Sewing',
-       'USER': 'postgres',
-       'PASSWORD': '5052',
-       'HOST': 'localhost',
-       'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #  'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME':  'Sewing',
+    #    'USER': 'postgres',
+    #    'PASSWORD': '5052',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432'
+    # }
 }
 
 
